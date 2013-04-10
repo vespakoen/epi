a80e7fd2b1f38581fb876697b91d9258520fdcf0
Koen Schmeets <k.schmeets@gmail.com>
2013-04-10 14:59:00 +0200

    Adding comments

--- src/Vespakoen/EPI/EPI.php
+++ src/Vespakoen/EPI/EPI.php
@@ -9,9 +9,19 @@ use Vespakoen\EPI\Relations\HasMany;
 
 class EPI {
 
+	/**
+	 * The relationships to eagerload
+	 * 
+	 * @var array
+	 */
 	public $eagerLoad = array();
 
-	public $select = array();
+	/**
+	 * The columns to load
+	 * 
+	 * @var array
+	 */
+	private $select = array();
 
 	/**
 	 * All of the available clause operators.
@@ -22,22 +32,43 @@ class EPI {
 		'<=', '>=', '<>', '!=', '=', '<', '>'
 	);
 
+	/**
+	 * Create a new EPI instance
+	 * 
+	 * @param Illuminate\Database\Eloquent\Model $model The Eloquent model
+	 */
 	public function __construct($model)
 	{
 		$this->model = $model;
 		$this->query = $this->model->newQuery();
 	}
 
+	/**
+	 * Create a new EPI instance statically
+	 * 
+	 * @param string $model An Eloquent model's name (including namespace)
+	 */
 	public static function model($model)
 	{
 		return new static(new $model);
 	}
 
+	/**
+	 * Create a new EPI instance statically
+	 * 
+	 * @param Illuminate\Database\Eloquent\Model $model An Eloquent model instance
+	 */
 	public static function modelInstance($model)
 	{
 		return new static($model);
 	}
 
+	/**
+	 * Eagerload relationships
+	 * 
+	 * @param  array $relations The relation identifiers to load
+	 * @return Vespakoen\EPI\EPI
+	 */
 	public function with($relations)
 	{
 		$this->eagerLoad = $relations;
@@ -45,6 +76,11 @@ class EPI {
 		return $this;
 	}
 
+	/**
+	 * Get the results
+	 * 
+	 * @return Illuminate\Database\Eloquent\Collection
+	 */
 	public function get()
 	{
 		$this->applyEagerLoads();
@@ -56,11 +92,21 @@ class EPI {
 		return $this->query->get();
 	}
 
+	/**
+	 * Apply the eagerloads to the query
+	 * 
+	 * @return Vespakoen\EPI\EPI
+	 */
 	protected function applyEagerLoads()
 	{
 		$this->query->with($this->eagerLoad);
 	}
 
+	/**
+	 * Turn filters input into standardized array
+	 * 
+	 * @return array the filters
+	 */
 	protected function getFilters()
 	{
 		$rawFilters = Input::get('filter', array());
@@ -88,6 +134,11 @@ class EPI {
 		return $filters;
 	}
 
+	/**
+	 * Turn sorters input into standardized array
+	 * 
+	 * @return array the sorters
+	 */	
 	protected function getSorters()
 	{
 		$rawSorters = (array) Input::get('sort', array());
@@ -121,6 +172,12 @@ class EPI {
 		return $sorters;
 	}
 
+
+	/**
+	 * Collect the relationships that have to be joined to allow filtering and sorting
+	 * 
+	 * @return array The relationidentifiers
+	 */
 	protected function getRelationIdentifiers()
 	{
 		$filters = $this->getFilters();
@@ -150,6 +207,13 @@ class EPI {
 		return array_unique($relationIdentifiers);
 	}
 
+	/**
+	 * Filter the relationships that are already part of another relation
+	 * For example "categories" will not get added in
+	 * case "categories.translation" is already present.
+	 * 
+	 * @return array The relationidentifiers to load
+	 */
 	protected function getRelationIdentifiersToLoad()
 	{
 		$relationIdentifiers = $this->getRelationIdentifiers();
@@ -185,128 +249,12 @@ class EPI {
 		return $relationIdentifiersToLoad;
 	}
 
-	protected function getRelations()
-	{
-		$relationIdentifiersToLoad = $this->getRelationIdentifiersToLoad();
-
-		$relations = array();
-		foreach ($relationIdentifiersToLoad as $relationIdentifier)
-		{
-			$relations += $this->getRelationsByIdentifier($relationIdentifier);
-		}
-
-		return $relations;
-	}
-
-	protected function applyJoins()
-	{
-		$relations = $this->getRelations();
-		foreach ($relations as $relation)
-		{
-			$relation->applyJoin($this->query);
-		}
-	}
-
-	protected function applyFilters()
-	{
-		$relations = $this->getRelations();
-
-		$filters = $this->getFilters();
-		foreach ($filters as $relationIdentifier => $info)
-		{
-			list($column, $value) = $info;
-
-			if( ! is_int($relationIdentifier))
-			{
-				$relation = $relations[$relationIdentifier];
-				$table = $relation->getTable();
-			}
-			else
-			{
-				$table = $this->model->getTable();
-			}
-
-			if(Str::startsWith($value, '%') || Str::endsWith($value, '%'))
-			{
-				$operator = 'LIKE';
-			}
-			elseif(Str::startsWith($value, $this->operators))
-			{
-				foreach ($this->operators as $operator)
-				{
-					if(Str::startsWith($value, $operator))
-					{
-						break;
-					}
-				}
-
-				$value = substr($value, strlen($operator));
-			}
-			else
-			{
-				$operator = '=';
-			}
-
-			$this->query->where($table.'.'.$column, $operator, $value);
-		}
-	}
-
-	protected function applySorters()
-	{
-		$relations = $this->getRelations();
-
-		$sorters = $this->getSorters();
-		foreach ($sorters as $relationIdentifier => $info)
-		{
-			list($column, $order) = $info;
-
-			if( ! is_int($relationIdentifier))
-			{
-				$relation = $relations[$relationIdentifier];
-				$table = $relation->getTable();
-			}
-			else
-			{
-				$table = $this->model->getTable();
-			}
-
-			$this->select[] = $table.'.'.$column.' AS sort_'.$column;
-			
-			$this->query->orderBy($table.'.'.$column, $order);
-		}
-	}
-
-	protected function applyRestrictions()
-	{
-		$this->query->distinct();
-		$this->query->select(array_merge($this->select, array($this->model->getTable().'.*')));
-
-		if(Input::has('offset'))
-		{
-			$offset = Input::get('offset');
-			$this->query->skip($offset);
-		}
-
-		if(Input::has('limit'))
-		{
-			$limit = Input::get('limit');
-			$this->query->take($limit);
-		}
-
-		if(Input::has('page'))
-		{
-			$perPage = Input::get('perpage', 25);
-			$page = Input::get('page');
-			$this->query->skip($page);
-		}
-
-		if(Input::has('perpage'))
-		{
-			$perPage = Input::get('perpage');
-			$this->query->take($perPage);
-		}
-	}
-
+	/**
+	 * Get information about a relationship by a identifier
+	 * 
+	 * @param  string $relationIdentifier The relation identifier (ex: categories.translation)
+	 * @return array the relationships
+	 */
 	protected function getRelationsByIdentifier($relationIdentifier)
 	{
 		$parent = null;
@@ -363,6 +311,153 @@ class EPI {
 		}
 
 		return $relations;
+	}
+
+	/**
+	 * Load information about relationships that are needed for joins and filters
+	 * 
+	 * @return array The relations
+	 */
+	protected function getRelations()
+	{
+		$relationIdentifiersToLoad = $this->getRelationIdentifiersToLoad();
+
+		$relations = array();
+		foreach ($relationIdentifiersToLoad as $relationIdentifier)
+		{
+			$relations += $this->getRelationsByIdentifier($relationIdentifier);
+		}
+
+		return $relations;
+	}
+
+	/**
+	 * Apply joins to the query
+	 * 
+	 * @return Void
+	 */
+	protected function applyJoins()
+	{
+		$relations = $this->getRelations();
+		foreach ($relations as $relation)
+		{
+			$relation->applyJoin($this->query);
+		}
+	}
+
+	/**
+	 * Apply filters to the query
+	 * 
+	 * @return Void
+	 */
+	protected function applyFilters()
+	{
+		$relations = $this->getRelations();
+
+		$filters = $this->getFilters();
+		foreach ($filters as $relationIdentifier => $info)
+		{
+			list($column, $value) = $info;
+
+			if( ! is_int($relationIdentifier))
+			{
+				$relation = $relations[$relationIdentifier];
+				$table = $relation->getTable();
+			}
+			else
+			{
+				$table = $this->model->getTable();
+			}
+
+			if(Str::startsWith($value, '%') || Str::endsWith($value, '%'))
+			{
+				$operator = 'LIKE';
+			}
+			elseif(Str::startsWith($value, $this->operators))
+			{
+				foreach ($this->operators as $operator)
+				{
+					if(Str::startsWith($value, $operator))
+					{
+						break;
+					}
+				}
+
+				$value = substr($value, strlen($operator));
+			}
+			else
+			{
+				$operator = '=';
+			}
+
+			$this->query->where($table.'.'.$column, $operator, $value);
+		}
+	}
+
+	/**
+	 * Apply sorters to the query
+	 * 
+	 * @return Void
+	 */
+	protected function applySorters()
+	{
+		$relations = $this->getRelations();
+
+		$sorters = $this->getSorters();
+		foreach ($sorters as $relationIdentifier => $info)
+		{
+			list($column, $order) = $info;
+
+			if( ! is_int($relationIdentifier))
+			{
+				$relation = $relations[$relationIdentifier];
+				$table = $relation->getTable();
+			}
+			else
+			{
+				$table = $this->model->getTable();
+			}
+
+			$this->select[] = $table.'.'.$column.' AS sort_'.$column;
+			
+			$this->query->orderBy($table.'.'.$column, $order);
+		}
+	}
+
+	/**
+	 * Apply restrictions to the query
+	 * 
+	 * @return Void
+	 */
+	protected function applyRestrictions()
+	{
+		$this->query->distinct();
+		$this->query->select(array_merge($this->select, array($this->model->getTable().'.*')));
+
+		if(Input::has('offset'))
+		{
+			$offset = Input::get('offset');
+			$this->query->skip($offset);
+		}
+
+		if(Input::has('limit'))
+		{
+			$limit = Input::get('limit');
+			$this->query->take($limit);
+		}
+
+		if(Input::has('page'))
+		{
+			$perPage = Input::get('perpage', 25);
+			$page = Input::get('page');
+			$this->query->skip($page);
+		}
+
+		if(Input::has('perpage'))
+		{
+			$perPage = Input::get('perpage');
+			$this->query->take($perPage);
+		}
 	}
 
 }
