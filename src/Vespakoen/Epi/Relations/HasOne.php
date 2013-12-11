1eb941d5d863d8dc663355e005fc29bb9f877e64
Koen Schmeets <k.schmeets@gmail.com>
2013-12-11 01:18:41 +0100

    Adding HasOne relationship

---
+++ src/Vespakoen/Epi/Relations/HasOne.php
@@ -0,0 +1,44 @@
+<?php namespace Vespakoen\Epi\Relations;
+
+use Vespakoen\Epi\Manipulators\Join;
+use Vespakoen\Epi\Interfaces\RelationInterface;
+
+use Illuminate\Database\Eloquent\Relations\Relation;
+
+class HasOne implements RelationInterface {
+
+	public function make($parent = null, Relation $relation = null, $relationIdentifier)
+	{
+		$this->parent = $parent;
+		$this->relation = $relation;
+		$this->relationIdentifier = $relationIdentifier;
+
+		return $this;
+	}
+
+	public function getJoins()
+	{
+		$table = $this->relation
+			->getModel()
+			->getTable();
+
+		$firstTable = $this->parent
+			->getTable();
+		$firstColumn = $this->parent->getKeyName();
+
+		$secondTableAndColumn = $this->relation
+			->getForeignKey();
+
+		return array(
+			Join::make($table, $firstTable.'.'.$firstColumn, '=', $secondTableAndColumn)
+		);
+	}
+
+	public function getTable()
+	{
+		return $this->relation
+			->getModel()
+			->getTable();
+	}
+
+}
