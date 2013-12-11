f9dfbb3a0f79ceb5d9e365e16223b1102b7e0d6c
Koen Schmeets <k.schmeets@gmail.com>
2013-12-11 01:21:07 +0100

    Implementing  SorterExtractor

--- src/Vespakoen/Epi/Extractors/SorterExtractor.php
+++ src/Vespakoen/Epi/Extractors/SorterExtractor.php
@@ -1,12 +1,46 @@
 <?php namespace Vespakoen\Epi\Extractors;
 
+use Vespakoen\Epi\Manipulators\Sorter;
 use Vespakoen\Epi\Interfaces\Extractors\SorterExtractorInterface;
 
 class SorterExtractor extends Extractor implements SorterExtractorInterface {
 
 	public function extract(array $input)
 	{
-		return array();
+		// where in the input should we look?
+		$key = $this->config['keys']['sort'];
+
+		// is there a sort at all?
+		if( ! array_key_exists($key, $input))
+		{
+			return array();
+		}
+
+		// make some sorters!
+		$sorters = array();
+
+		if(is_string($input[$key]))
+		{
+			list($relationIdentifier, $column) = $this->extractRelationIdentifierAndColumn($input[$key]);
+			$direction = 'asc';
+
+			$table = $this->getSafeAliasedTableName($relationIdentifier);
+
+			$sorters[] = Sorter::make($relationIdentifier, $table, $column, $direction);
+
+			return $sorters;
+		}
+
+		foreach($input[$key] as $rawRelationIdentifierAndColumn => $direction)
+		{
+			list($relationIdentifier, $column) = $this->extractRelationIdentifierAndColumn($rawRelationIdentifierAndColumn);
+
+			$table = $this->getSafeAliasedTableName($relationIdentifier);
+
+			$sorters[] = Sorter::make($relationIdentifier, $table, $column, $direction);
+		}
+
+		return $sorters;
 	}
 
 }
