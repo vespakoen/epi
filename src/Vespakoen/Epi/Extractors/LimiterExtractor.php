5d6b7bbd35bc85c6368d44bd96433cef73b6bce4
Koen Schmeets <k.schmeets@gmail.com>
2013-12-11 01:21:17 +0100

    Implementing  LimiterExtractor

--- src/Vespakoen/Epi/Extractors/LimiterExtractor.php
+++ src/Vespakoen/Epi/Extractors/LimiterExtractor.php
@@ -1,12 +1,27 @@
 <?php namespace Vespakoen\Epi\Extractors;
 
+use Vespakoen\Epi\Manipulators\Limiter;
 use Vespakoen\Epi\Interfaces\Extractors\LimiterExtractorInterface;
 
 class LimiterExtractor extends Extractor implements LimiterExtractorInterface {
 
 	public function extract(array $input)
 	{
-		return array();
+		// where in the input should we look?
+		$skipKey = $this->config['keys']['skip'];
+		$takeKey = $this->config['keys']['take'];
+
+		// is there a filter at all?
+		if( ! array_key_exists($skipKey, $input) && ! array_key_exists($takeKey, $input))
+		{
+			return array();
+		}
+
+		$limiters = array();
+
+		$limiters[] = Limiter::make(array_get($input, $skipKey, 0), array_get($input, $takeKey, 25));
+
+		return $limiters;
 	}
 
 }
