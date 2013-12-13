a3a30248c8f9577e7d417805aae4fb7901845911
Koen Schmeets <k.schmeets@gmail.com>
2013-12-13 01:31:32 +0100

    Adding MorphOne relationship

---
+++ src/Vespakoen/Epi/Relations/MorphOne.php
@@ -0,0 +1,73 @@
+<?php namespace Vespakoen\Epi\Relations;
+
+use Vespakoen\Epi\Facades\Join;
+use Vespakoen\Epi\Interfaces\RelationInterface;
+
+use Illuminate\Database\Eloquent\Relations\Relation as LaravelRelation;
+
+class MorphOne extends Relation implements RelationInterface {
+
+	public function make($parent = null, LaravelRelation $relation = null, $relationIdentifier)
+	{
+		$this->parent = $parent;
+		$this->relation = $relation;
+		$this->relationIdentifier = $relationIdentifier;
+
+		return $this;
+	}
+
+	public function getJoins()
+	{
+		$relationIdentifier = $this->relationIdentifier;
+		$firstTable = $this->getFirstTable();
+		$firstColumn = $this->getFirstColumn();
+		$secondTable = $this->getSecondTable();
+		$secondColumn = $this->getSecondColumn();
+
+		$join = $this->app->make('epi::manipulators.join');
+		return array(
+			$join->make($relationIdentifier, $firstTable, $firstColumn, '=', $secondTable, $secondColumn)
+		);
+	}
+
+	public function getTable()
+	{
+		return $this->relation
+			->getModel()
+			->getTable();
+	}
+
+	protected function getFirstTable()
+	{
+		$table = $this->relation->getParent()
+			->getTable();
+
+		return $table;
+	}
+
+	protected function getFirstColumn()
+	{
+		$key = $this->parent->getKeyName();
+
+		return $key;
+	}
+
+	protected function getSecondTable()
+	{
+		$secondTableAndColumn = $this->relation
+			->getForeignKey();
+		list($table, $key) = explode('.', $secondTableAndColumn);
+
+		return $table;
+	}
+
+	protected function getSecondColumn()
+	{
+		$secondTableAndColumn = $this->relation
+			->getForeignKey();
+		list($table, $key) = explode('.', $secondTableAndColumn);
+
+		return $key;
+	}
+
+}
