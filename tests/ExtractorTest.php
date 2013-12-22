c044d46d9c870811f132e9b8e0778c38ca3ba512
Koen Schmeets <k.schmeets@gmail.com>
2013-12-22 15:38:07 +0100

    Fixing test

--- tests/ExtractorTest.php
+++ tests/ExtractorTest.php
@@ -123,23 +123,4 @@ class ExtractorTest extends EpiTests {
 		$this->assertEquals($take, $input['take']);
 	}
 
-	public function testCanExtractJoins()
-	{
-		$input = array(
-			'filter' => array(
-				'name' => 'koen'
-			)
-		);
-		$filters = $this->filterExtractor->extract($input);
-
-		$input = array(
-			'sort' => array(
-				'name' => 'asc'
-			)
-		);
-		$sorters = $this->sorterExtractor->extract($input);
-
-		$joins = $this->joinExtractor->extract($filters, $sorters);
-	}
-
 }
