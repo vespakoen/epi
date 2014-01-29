111180f72dec085bd1a37465f8a11cf6152061c8
Koen Schmeets <k.schmeets@gmail.com>
2014-01-29 19:03:47 +0100

    Another attempt at making the Ember response work, not worky yet though...

--- src/Vespakoen/Epi/Adaptors/Ember.php
+++ src/Vespakoen/Epi/Adaptors/Ember.php
@@ -2,67 +2,100 @@
 
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Support\Str;
 
 class Ember extends Adaptor {
 
 	public $response = array();
 
-	public function getIdentifierFromModel($model)
+	public function getIdentifierFromModel($model, $parentModel = null)
 	{
 		$modelName = get_class($model);
 		$parts = explode('\\', $modelName);
+
+        if(get_class($model) == get_class($parentModel))
+        {
+            return null;
+        }
+
+		if($model instanceof Pivot)
+		{
+			return $model->getTable();
+		}
+
 		return strtolower(array_pop($parts));
 	}
 
-	public function process($data, $relationIdentifier = null, $parentModel = null)
+	public function process($data, $parentIdentifier = null, $parentModel = null, $isCollection = false)
 	{
 		$ids = array();
 		if($data instanceof Collection)
 		{
 			foreach($data as $model)
 			{
-
-				$ids[] = $model->getKey();
-
-				$this->process($model);
+				$this->process($model, $parentIdentifier, $parentModel, true);
 			}
-		}
+        }
 		elseif($data instanceof Model)
 		{
 			$key = $data->getKey();
-			$ids = $key;
 
-			foreach($data->getRelations() as $identifier => $collection)
+			foreach($data->getRelations() as $relationIdentifier => $collection)
 			{
-				if($identifier == 'pivot') continue;
-
-				$this->process($collection, $identifier, $data);
+				$this->process($collection, $relationIdentifier, $data);
 			}
 
-			$identifier = $this->getIdentifierFromModel($data);
+			$resolvedIdentifier = $this->getIdentifierFromModel($data, $parentModel);
+            $identifier = is_null($resolvedIdentifier) ? $parentIdentifier : $resolvedIdentifier;
 			if( ! isset($this->response[$identifier][$key]))
 			{
 				$this->response[$identifier][$key] = $data->toArray();
 			}
 			elseif( ! is_null($parentModel))
 			{
-				if(isset($this->response[$identifier][$key][$this->getIdentifierFromModel($parentModel)]))
-				{
-					$this->response[$identifier][$key][$this->getIdentifierFromModel($parentModel)] = array_merge($this->response[$identifier][$key][$this->getIdentifierFromModel($parentModel)], $ids);
-				}
-				else
-				{
-					$this->response[$identifier][$key][$this->getIdentifierFromModel($parentModel)] = $ids;
-				}
-			}
-		}
+                if($isCollection)
+                {
+                    $ids = array($key);
+                    $column = $resolvedIdentifier.'_ids';
+                }
+                else
+                {
+                    $ids = $key;
+                    $column = $resolvedIdentifier.'_id';
+                }
 
-		if( ! is_null($parentModel))
-		{
-			$parentModel->setRelation($relationIdentifier, false);
-			$identifier = $this->getIdentifierFromModel($data).'Ids';
-			$parentModel->$identifier = $ids;
-		}
+                if($isCollection && isset($this->response[$identifier][$key][$column]))
+                {
+                    $this->response[$identifier][$key][$column] = array_merge($this->response[$identifier][$key][$column], array($ids));
+                }
+                elseif( ! isset($this->response[$identifier][$key][$column]))
+                {
+                    $this->response[$identifier][$key][$column] = $ids;
+                }
+			}
+
+            if( ! is_null($parentModel))
+            {
+                $parentModel->setRelation($parentIdentifier, false);
+            }
+
+            if( ! is_null($parentModel) && ! $isCollection)
+            {
+                $column = $parentIdentifier.'_id';
+                $parentModel->$column = $ids;
+            }
+        }
+        else
+        {
+            if( ! is_null($parentModel))
+            {
+                $parentModel->setRelation($parentIdentifier, false);
+
+                $column = Str::singular($parentIdentifier).'_id';
+                $parentModel->$column = null;
+            }
+        }
 
 		return $this->response;
 	}
